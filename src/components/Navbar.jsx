63ac9ff02810f6873b2ec81bbd2c43c29486d758
ssatyamm07 <satyamraj151.rajgir@gmail.com>
2024-02-26 23:58:15 +0530

    project started

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+function Navbar() {
+  return (
+    <>
+    <div>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste aliquid fuga quisquam repudiandae impedit ipsum, neque vero, ex asperiores quasi facere consequatur? Culpa, tempora ullam quae obcaecati dolorum quia rem?</p>
+    </div>
+    </>
+  )
+}
+
+export default Navbar;
